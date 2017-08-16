4d8eee8904f3e2ac3ad533c8fa29aea0abb7604b
VI <moccababy777@gmail.com>
2017-08-16 16:24:20 +1200

    AWS-Project

---
+++ inc/Config.inc.php
@@ -0,0 +1,18 @@
+<?php
+
+define('HOST','127.0.0.1');
+define('DATABASE_NAME','HatDB');
+define('USER_NAME','root');
+define('PWD','112233');
+
+define('PAGE_SIZE',10);
+define('LOG_PATH','../Log/');
+define('IMG_PATH','HatImages');
+define('CUSTOMER_PASS','available');
+define('CUSTOMER_DENY','deny');
+define('ORDER_SHIPPED','shipped');
+define('ORDER_WAITING','waiting');
+define('GST','15');
+define('SESSION_CART','SESSION_CART');
+error_reporting("E_ALL"); //只显示错误和警告，不显示 注意的错误 //| E_WARNING
+?>
